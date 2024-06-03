28093573579cd5667d5a6d20346d70e4ab38a7db
Kenneth Cochran <kecochran143@gmail.com>
2024-06-03 12:28:38 -0400

    initialized terraform files, added aws_iam_policy and gave control over EC2 resources and actions, updated jenkinsServer instance to use the iam_instance_profile, added .gitignore

--- jenkins.tf
+++ jenkins.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.52.0"
     }
   }
@@ -12,47 +12,47 @@ provider "aws" {
 }
 
 data "aws_vpc" "jenkinsVPC" {
- default = true
+  default = true
 }
 
 resource "aws_security_group" "jenkins_server_sg_tf" {
- name        = "jenkins_server_sg_tf"
- description = "Allow SSH to jenkins server"
- vpc_id      = data.aws_vpc.jenkinsVPC.id
+  name        = "jenkins_server_sg_tf"
+  description = "Allow SSH to jenkins server"
+  vpc_id      = data.aws_vpc.jenkinsVPC.id
 
-ingress {
-   description = "SSH ingress"
-   from_port   = 22
-   to_port     = 22
-   protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-ingress {
-   description = "HTTP ingress"
-   from_port   = 80
-   to_port     = 80
-   protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-ingress {
-   description = "HTTP ingress"
-   from_port   = 8080
-   to_port     = 8080
-   protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-egress {
-   from_port   = 0
-   to_port     = 0
-   protocol    = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+  ingress {
+    description = "SSH ingress"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP ingress"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP ingress"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_iam_role" "jenkinsRole" {
-    name = "jenkinsRole"
+  name = "jenkinsRole"
 
-    assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
 {
         "Version": "2012-10-17",
         "Statement": [
@@ -70,15 +70,56 @@ EOF
 }
 
 resource "aws_iam_instance_profile" "jenkinsProfile" {
-    name = "jenkinsProfile"
-    
+  name = "jenkinsProfile"
+  role = aws_iam_role.jenkinsRole.name
+}
+
+resource "aws_iam_policy" "EC2_iam_policy" {
+  name   = "EC2_iam_policy"
+  policy = <<EOF
+ {
+    "Version": "2012-10-17", 
+    "Statement": [ 
+        { 
+            "Effect": "Allow",
+            "Action": [ 
+                "ec2:RunInstances", 
+                "ec2:TerminateInstances", 
+                "ec2:StartInstances",
+                "ec2:StopInstances"
+            ],
+            "Resource": "*" 
+        }, 
+        { 
+            "Effect": "Allow", 
+            "Action": [ 
+                "ec2:CreateTags", 
+                "ec2:DescribeInstances",
+                "ec2:DescribeInstanceStatus",
+                "ec2:DescribeAddresses", 
+                "ec2:AssociateAddress",
+                "ec2:DisassociateAddress",
+                "ec2:DescribeRegions",
+                "ec2:DescribeAvailabilityZones"
+            ], 
+            "Resource": "*" 
+        } 
+    ] 
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "jenkinsRoleAttachment" {
+  role       = aws_iam_role.jenkinsRole.name
+  policy_arn = aws_iam_policy.EC2_iam_policy.arn
 }
 
 resource "aws_instance" "jenkinsServer" {
-  ami           = ami-0a3c3a20c09d6f377
-  instance_type = "t3a.medium"
-  key_name = "son"
+  ami                    = "ami-0a3c3a20c09d6f377"
+  instance_type          = "t3a.medium"
+  key_name               = "son"
   vpc_security_group_ids = [aws_security_group.jenkins_server_sg_tf.id]
+  iam_instance_profile   = aws_iam_instance_profile.jenkinsProfile.id
 
   tags = {
     Name = "jenkinsServer"
